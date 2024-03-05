fff6b58dc62ffd5b9297966d5ff107be4593345a
joywins25 <joywins25@gmail.com>
2024-03-05 14:16:03 +0900

    fixed an error 500, strict-origin-when-cross-origin at 'src/routes/movie/[id]/+page.svelte' in using the api of details

--- src/routes/movie/[id]/+page.svelte
+++ src/routes/movie/[id]/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 export let data;
+console.log('>> data : ', data);
 </script>
 
 <h1>Movie Detail</h1>
@@ -9,7 +10,13 @@ export let data;
         미해결과제 : * ExA005_MovieApp_Failed001.an error 500, strict-origin-when-cross-origin :          
     -->
     <div class="txt-container">
-        <h1>{data.title}</h1>
-        <!-- <p class="overview">{data.overview}</p> -->
+        {#if data.props.data && data.props.data.title}
+            <h2>{data.props.data.title}</h2>
+            <!-- You can also display other movie details here -->
+            <img src={"https://media.themoviedb.org/t/p/w780/" + data.props.data.backdrop_path} alt={data.props.data.title}>
+            <h3>{data.props.data.title}</h3>
+        {:else}
+            <p>Loading...</p>
+        {/if}
     </div>
 </div>
