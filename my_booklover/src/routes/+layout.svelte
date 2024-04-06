41d085f5bea49b88e7ab5680b7e5eb795ac55920
joywins25 <joywins25@gmail.com>
2024-04-06 21:27:59 +0900

    ~Ch04.07.Messages :use store and css class conditionally

--- my_booklover/src/routes/+layout.svelte
+++ my_booklover/src/routes/+layout.svelte
@@ -5,8 +5,9 @@
 
     onMount(()=>{
         setTimeout(()=>{
-            messageStore.showSuccess('성공입니다');
-            // messageStore.showError('처리에 실패했습니다')
+            // messageStore.showSuccess('성공입니다');
+            messageStore.showError('처리에 실패했습니다')
+            console.log('messageStore.type : ', $messageStore.type);
         }, 3000);
     })
 
@@ -26,10 +27,10 @@
     <div class="wrapper">
         {#if $messageStore.show}
             <!-- HTML Snippets :: 10 - Message Store :: Alert HTML -->
-            <article class:article_error={$messageStore.type === 'error'}
-                     class:article_success={$messageStore.type === 'success'}>
-                <header class:header_error={$messageStore.type === 'error'}
-                        class:header_success={$messageStore.type === 'success'}>
+            <article class:article_error={$messageStore.type.toLowerCase() === 'error'}
+                     class:article_success={$messageStore.type.toLowerCase() === 'success'}>
+                <header class:header_error={$messageStore.type.toLowerCase() === 'error'}
+                        class:header_success={$messageStore.type.toLowerCase() === 'success'}>
                     <strong>{$messageStore.type}:</strong>
                 </header>
                 <div>
@@ -44,36 +45,29 @@
 </main>
 
 <style>
-    .wrapper {
-        padding-top: 20px;
-    }
     article {
         display: flex;
         flex-direction: column;
         justify-content: center; /* Vertically center the content */
         align-items: center; /* Horizontally center the content */    
-    }
+    } 
 
-    .header_error {
+    .wrapper {
+        padding-top: 20px;
+    }
+    header.header_error {
         color: yellow;
         background-color: red;
     }
-    .article_error {
+    article.article_error {
         background-color: red;
     }
-    .header_success {
+    header.header_success {
         color: yellow;
         background-color: green;
     }
-    .article_success {
+    article.article_success {
         background-color: green;
-    }
-    .header_hide {
-        color: yellow;
-        background-color: blue;
-    }
-    .article_hide {
-        background-color: blue;
     }
 
     .article_close_button {
